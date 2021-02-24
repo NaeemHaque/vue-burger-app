5cb37c28cf9692d776e3930d780a47df089b2886
NaemHaque <naeemhaque248@gmail.com>
2021-02-24 23:37:53 +0600

    init

---
+++ src/components/sidebar.vue
@@ -0,0 +1,40 @@
+<template>
+<v-row class="primary">
+  <div class="sidebar primary">
+    <object  data="icon/menu.svg"></object>
+    <div>
+      <v-icon>facebook</v-icon>
+    </div>
+    <object  data="icon/logout.svg"></object>
+  </div>
+</v-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scopped>
+.sidebar{
+    overflow: hidden;
+    justify-self:  left;
+    grid-area: sidebar;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.sidebar > div object{
+margin-bottom: 2em;
+}
+
+.sidebar object{
+    width:25px;
+    height: 25px;
+    margin: 1em;
+    display: block;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  vuetify,
+  render: h => h(App),
+  themes: {
+    primary: '#1976D2',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00'
+  }
 }).$mount('#app')
