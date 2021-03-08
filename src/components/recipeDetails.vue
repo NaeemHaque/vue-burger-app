84035384a97e15107ed6e744575fd369fba1ae4a
NaemHaque <naeemhaque248@gmail.com>
2021-03-08 22:55:48 +0600

    init

--- src/components/recipeDetails.vue
+++ src/components/recipeDetails.vue
@@ -1,25 +1,4 @@
-<template>
-  <v-card
-    class="mx-auto"
-    max-width="600"
-  >
-  <v-card-title>Burger Name</v-card-title>
-    <v-img
-      class="white--text align-end"
-      height="200px"
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-    >
-    </v-img>
-
-    <v-card-text class="text--primary">
-      <div class="title">Recipe</div>
-
-      <div>Upcoming After adding Firebase..</div>
-    </v-card-text>
-
-  </v-card>
-</template>
-<!--
+-->
 <template>
 <v-container>
 
@@ -38,21 +17,21 @@
     </v-row>
 </v-container>
 </template>
--->
+
 <script>
 
 export default {
   props: ['id'],
   data () {
     return {
-      recipes: { id: 1, title: 'Title', img: 'burger-5.jpg' }
-      // recipes: [
-      //   { id: 1, title: 'b1' },
-      //   { id: 2, title: 'b2' },
-      //   { id: 3, title: 'b3' },
-      //   { id: 4, title: 'b4' }
+      // recipes: { id: 1, title: 'Title', img: 'burger-5.jpg' }
+      recipes: [
+        { id: 1, title: 'b1' },
+        { id: 2, title: 'b2' },
+        { id: 3, title: 'b3' },
+        { id: 4, title: 'b4' }
 
-      // ]
+      ]
     }
   }
 }
